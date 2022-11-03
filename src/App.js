512be82ba914168b89e7f7264d88d117f2a416e0
shehandilusanka97 <dilumzsanox9@gmail.com>
2022-11-04 01:29:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home/Home';
-import PortfolioPage from './PortfolioPage/PortfolioPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import PortfolioPage from "./PortfolioPage/PortfolioPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Home/>}/>
-        <Route path="/portfolio" exact element={<PortfolioPage/>}/>
-
-      </Routes>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/portfolio" exact element={<PortfolioPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
